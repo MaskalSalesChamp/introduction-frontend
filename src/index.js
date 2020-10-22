4118b720a6163dd237ea58668d2fac38c8931717
marianmaskal <marian.maskal@yahoo.com>
2020-10-22 16:26:35 +0200

    Updated patch request, buttonView, formView

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import searchTemp from './templates/searchView.hbs'
 import appLayoutTemp from './templates/appLayoutView.hbs'
 import listItemTemp from './templates/ListItemView.hbs'
 import adressTemplate from './templates/adressTemplate.hbs'
-import formTemp from './templates/formTemplate.hbs';
+import formTemp from './templates/formTemplate.hbs'
 //import { AdressModel } from './model/adressModel.js';
 
 import '@fortawesome/fontawesome-free/js/all.js'
@@ -30,31 +30,32 @@ var AdressCollection = Backbone.Collection.extend({
   }
 })
 
+/*var AdressModel = Backbone.Model.extend({
+  url:'https://introduction-api.do.saleschamp.io/introduction-api/items/address/1', 
+}); */
+
 var State = Backbone.Model.extend({
+  url:
+    'https://introduction-api.do.saleschamp.io/introduction-api/items/address',
   defaults: {
     selectedAdress: null
-  },
-});
-var FormView = View.extend({
-  template: formTemp,
-  regions: {
   }
 })
 
 var HeaderView = View.extend({
   template: headerTemp,
   initialize (model) {
-    this.labelAdress = this.model.get('selectedAdress') || 'Neznama ulica';
-    this.listenTo(this.model, 'change:selectedAdress', this.onStreetChange);
-    //this.model.on('change:street',this.reRender);  
-    //console.log(this.model);
+    this.labelAdress = this.model.get('selectedAdress') || 'Neznama ulica'
+    this.listenTo(this.model, 'change:selectedAdress', this.onStreetChange)
 
+    //this.model.on('change:street',this.reRender);
+    console.log(model)
   },
-  onStreetChange() {
+  onStreetChange () {
     /*console.log('CHANGEEEEEE!!');
     console.log(this.model.get('selectedAdress').attributes.street);*/
-    this.labelAdress = this.model.get('selectedAdress').attributes.street;
-    this.render();
+    this.labelAdress = this.model.get('selectedAdress').attributes.street
+    this.render()
   },
   templateContext () {
     return {
@@ -62,22 +63,37 @@ var HeaderView = View.extend({
     }
   }
 })
+var FormView = View.extend({
+  template: formTemp,
+  ui: {
+    cancel: '#cancel',
+    save: '#save'
+  },
+  triggers: {
+    'click @ui.cancel': 'onCancelClick',
+    'click @ui.save': 'onSaveClick'
+  }
+})
+
 var ButtonsView = View.extend({
   template: buttonsTemp,
   triggers: {
     'click @ui.nt': 'onNTClick',
     'click @ui.ovk': 'onOVKClick'
   },
+  events: {
+    'click @ui.ovk': 'NTClicked',
+    'click @ui.ovk': 'OVKClicked'
+  },
   ui: {
     nt: '#NT',
     ovk: '#OVK'
   },
   initialize () {
-    this.listenTo(this.model, 'change:selectedAdress', this.onStreetChange);
+    this.listenTo(this.model, 'change:selectedAdress', this.onStreetChange)
   },
-  onStreetChange(){
-  },
-  /*onNTClick() {
+  onStreetChange () {}
+  /*onNTClick() { 
     var attributes = Backbone.Model.extend({});
     attributes = this.model.get('selectedAdress').attributes;
     //var status = this.model.get('selectedAdress');
@@ -102,41 +118,48 @@ var SearchView = View.extend({
   }
 })
 
-
 var ListView = CollectionView.extend({
-  initialize () {
-    //console.log(this.options.model);
-  },
+  initialize () {},
   tagName: 'ul',
   className: 'list-group',
   childViewOptions (model) {
     //console.log(model);
     //console.log(model.get('adresses'))
-    return { collection: model.get('adresses'),state: this.options.model}
+    return { collection: model.get('adresses'), state: this.options.model }
   },
 
   childView: CollectionView.extend({
-    tagName:'li',
-      className: 'list-group-item',
+    tagName: 'li',
+    className: 'list-group-item',
     childViewEvents: {
       'select:item': 'itemSelected'
     },
     itemSelected (view) {
-      var stateModel = this.options.state;
-      this.$el.toggleClass('active');
-      this.toggleElement = this.$el[0];
+      var stateModel = this.options.state
+      this.$el.toggleClass('active')
+      this.toggleElement = this.$el[0]
 
-      stateModel.set('selectedAdress', view.model);
-      console.log(stateModel.get('selectedAdress').attributes.street);
-
+      stateModel.set('selectedAdress', view.model)
+      //console.log(stateModel.get('selectedAdress').attributes.street);
+      stateModel.set('collection', this.model.collection)
+      console.log(stateModel)
     },
     childView: View.extend({
-      tagName:'span',
-      initialize () {},
+      tagName: 'span',
+      template: adressTemplate,
+      initialize () {
+        //console.log(this.model.attributes.number);
+        //console.log(this.collection);
+      },
       triggers: {
         'click span': 'select:item'
       },
-      template: adressTemplate,
+      events: {
+        'click span': 'onSelect'
+      },
+      onSelect () {
+        $('.numberList').toggleClass('d-none')
+      }
     })
   })
 })
@@ -149,25 +172,64 @@ const MyView = View.extend({
     header: '#header-region',
     buttons: '#buttons-region',
     search: '#search-region',
-    list: '#list-region' 
+    list: '#list-region'
   },
   childViewEvents: {
-    'onNTClick': 'NTClicked',
-    'onOVKClick': 'OVKClicked'
-  },
+    onNTClick: 'NTClicked',
+    onOVKClick: 'OVKClicked',
+    onSaveClick: 'onSave',
+    onCancelClick: 'onCancel'
 
-  NTClicked() {
-    this.model.save('selectedAdress.attributes.status', 'NT', {url:"https://introduction-api.do.saleschamp.io/introduction-api/items/address/"+ this.model.get('selectedAdress').attributes.id, patch: true});
-    console.log(this.model.get('selectedAdress'));
   },
-  OVKClicked() {
-    this.showChildView('mainRegion', new FormView());
+  onSave () {
+    console.log('SAVE??')
+  },
+  onCancel() {
+    console.log('CANCEL??')
+  },
+  NTClicked () {
+    //console.log(this.model.get('selectedAdress').collection);
+    /* this.model.save({status: 'NT'}, {patch:true, success(){
+      console.log('saved bitch');
+    }})
+    var activeAdressStatus = this.model.get('selectedAdress').attributes.status;
+    //console.log(activeAdress);
+    this.model.sync(activeAdressStatus,'NT',{patch:true, success(){
+      console.log('saved bitch');
+    }});
+    var adressModel = new AdressModel();
+    adressModel = this.model.get('selectedAdress');
+    console.log(adressModel);*/
+    var activeId = this.model.get('selectedAdress').id
+    console.log(activeId)
+    //console.log('https://introduction-api.do.saleschamp.io/introduction-api/items/address/' + activeId);
+
+    this.model.get('selectedAdress').save(
+      { status: 'NT' },
+      {
+        url:
+          'https://introduction-api.do.saleschamp.io/introduction-api/items/address/' +
+          activeId,
+        patch: true,
+        success () {
+          console.log('saved dopi')
+        }
+      }
+    )
+    /*adressModel.save({status: 'NT'}, {patch:true, success(){
+      console.log('saved bitch');
+    }})*/
+  },
+  OVKClicked () {
+    this.showChildView('mainRegion', new FormView({ model: this.model }))
   },
   onRender () {
-    var collection = new AdressCollection()
+    console.log(this.collection)
+    var collection = new AdressCollection({})
+    console.log(collection.url)
     collection.fetch({
       success: response => {
-        var streetCollection = new Backbone.Collection( 
+        var streetCollection = new Backbone.Collection(
           _.values(
             _.mapObject(
               collection.groupBy(adress => {
@@ -186,34 +248,35 @@ const MyView = View.extend({
             )
           )
         )
+        console.log(streetCollection)
         this.showChildView(
           'list',
-          new ListView({ collection: streetCollection, model:this.model})
-        )
+          new ListView({ collection: streetCollection, model: this.model })
+        ),
+          this.showChildView('buttons', new ButtonsView({ model: this.model }))
       }
     }),
-    this.showChildView(
-      'header',
-      new HeaderView({model:this.model})
-    ),
-    this.showChildView('buttons', new ButtonsView({model:this.model}))
-    this.showChildView('search', new SearchView())
-    this.listenTo(this.model, 'change',() => console.log('change made' + this.model))
-
-
-  },
-
+      this.showChildView('header', new HeaderView({ model: this.model })),
+      //this.showChildView('buttons', new ButtonsView({model:this.model}))
+      this.showChildView('search', new SearchView())
+    this.listenTo(this.model, 'change', () =>
+      console.log('change made' + this.model)
+    )
+  }
 })
 const App = Application.extend({
   region: '#app-container',
-  onBeforeStart(app, options) {
-    this.model = new State(options.data);
+  onBeforeStart (app, options) {
+    this.model = new State(options.data)
   },
 
   onStart (app) {
-    this.showView(new MyView({
-      model: this.model
-    }))
+    this.showView(
+      new MyView({
+        model: this.model
+      })
+    )
+    //console.log(this.model)
     //console.log(this.model.attributes.selectedAdress);
   }
 })
