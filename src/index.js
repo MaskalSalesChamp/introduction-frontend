e388b7df81d00030088822fc5895d2fdb9ab77b7
marianmaskal <marian.maskal@yahoo.com>
2020-10-27 14:43:38 +0100

    Updated ListView, state managment, selecting streetNumber

--- src/index.js
+++ src/index.js
@@ -47,7 +47,9 @@ var HeaderView = View.extend({
     this.listenTo(this.model, 'change:selectedAdress', this.onStreetChange)
   },
   onStreetChange () {
-    this.labelAdress = this.model.get('selectedAdress').attributes.street
+    this.labelAdress = this.model.get('selectedAdress').attributes.city + ', ' 
+    + this.model.get('selectedAdress').attributes.street 
+    + ' ' + this.model.get('selectedAdress').attributes.number
     this.render()
   },
   templateContext () {
@@ -73,7 +75,7 @@ var FormView = View.extend({
   triggers: {
     'click @ui.cancel': 'onCancelClick',
     'click @ui.save': 'onSaveClick',
-    'submit @ui.form': 'onSubmitClick'
+    'submit @ui.form': 'click:submit'
   },
   cancelling(){
   },
@@ -131,28 +133,29 @@ var SearchView = View.extend({
 })
 
 var ListView = CollectionView.extend({
-  regions: {
-    searchRegion: '#search-region',
-  },
-
-
+  //streetListView
   initialize () {
+    //console.log(this.options);
+
   },
   tagName: 'ul',
   className: 'list-group listSearch',
   childViewOptions (model) {
     //console.log(model);
-    //console.log(model.get('adresses'))
+    //console.log(model.get('adresses').models);
+    //console.log(_.each(_.pick(model.get('adresses').models, number));
+    var itemLabel = model.get('street');
+    var adressArray = model.get('adresses').models
     return { collection: model.get('adresses'), state: this.options.model }
   },
 
   childView: CollectionView.extend({
+    //streetView
     initialize() {
-      //console.log(this.options.model.get('street'));
-      //console.log(this.options.collection.model.street);
+      //console.log(this.options);
     },
     tagName: 'li',
-    className: 'list-group-item',
+    className: 'list-group-item streetItem',
     childViewEvents: {
       'select:item': 'itemSelected'
     },
@@ -161,30 +164,44 @@ var ListView = CollectionView.extend({
     },
     itemSelected (view) {
       var stateModel = this.options.state
-      this.$el.addClass('active');
-      this.$el.siblings().removeClass('active');
-      this.toggleElement = this.$el[0]
+      //console.log(view.model);
 
-      stateModel.set('selectedAdress', view.model)
-      //console.log(stateModel.get('selectedAdress').attributes.street);
-      stateModel.set('collection', this.model.collection)
-      //console.log(stateModel)
+      //stateModel.set('selectedAdress', view.model)
+    },
+    childViewOptions() {
+      return {state: this.options.state}
     },
     childView: View.extend({
+      //streetAdressView
+
       template: adressTemplate,
+      tagName: null,
+      className: 'list-group-item',
+
       initialize () {
+        //console.log(this.options.state);
         //console.log(this.model.attributes.number);
         //console.log(this.collection);
-        //console.log(this.options.model.id)
+        console.log(this.options.model);
       },
       triggers: {
         'click span': 'select:item'
       },
       events: {
+        'click li' : 'onNumberClick',
         'click span': 'onSelect'
       },
+      onNumberClick() {
+        var stateModel = this.options.state
+        console.log(stateModel);
+
+        stateModel.set('selectedAdress', this.options.model)
+        //stateModel.set('collection', this.model.collection)
+        this.$el.find('.numberItem').addClass('active');
+        this.$el.parents('.streetItem').siblings().find('.numberItem').removeClass('active')
+      },
       onSelect () {
-        this.$el.find('span').children().toggleClass('d-none');
+        this.$el.find('span').children().removeClass('d-none');
       }
     })
   })
@@ -206,33 +223,25 @@ const MyView = View.extend({
     onOVKClick: 'OVKClicked',
     onSaveClick: 'onSaved',
     onCancelClick: 'onCancel',
-    onSubmitClick: 'onSubmit',
+    'click:submit': 'onSubmit',
     onSearchKeyUp: 'onSearch'
   },
 
   onSearch(){
-    console.log(this.$el.find('.listSearch'));
+    console.log(this.$el.find('.listSearch').children('.streetItem').find('.daco'));
   },
 
   onSubmit() {
     console.log('submit??')
     console.log(this.model);
-    //this.showChildView('search', new SearchView());
     this.render();
-    this.onRender();
-    var coll = new AdressCollection();
-    coll.fetch({
-      success: res => {
-        console.log(res);
-      }
-    })
-
   },
   onCancel() {
     console.log('CANCEL??');
     //this.showChildView('search', new SearchView());
     this.$el.find('#search-region').removeClass('d-none');
     this.onRender();
+    console.log(this.model)
 
   },
   NTClicked () {
@@ -251,7 +260,6 @@ const MyView = View.extend({
           console.log('saved')
         }
       },
-      this.onRender()
     )
   },
   OVKClicked () {
@@ -303,7 +311,7 @@ const MyView = View.extend({
       //this.showChildView('buttons', new ButtonsView({model:this.model}))
       this.showChildView('search', new SearchView())
     this.listenTo(this.model, 'change', () => {
-      console.log('change made' + this.model)
+      console.log('change made')
 
     }
     )
@@ -330,11 +338,5 @@ const app = new App()
 
 app.start({
   data: {
-    /*id: 0,
-    city: 'unknown - city',
-    status: 'unknown - status',
-    street: 'unknown - street',
-    postalCode: 'unknown - postalCode',
-    isSelected: false*/
   }
 })
